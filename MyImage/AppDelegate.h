03fe483a3015e707d0df57c63d4b5b4c6a7ea3fa
刘虎 <liuhu@192.168.18.12>
2016-07-29 14:32:47 +0800

    Initial Commit

---
+++ MyImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyImage
+//
+//  Created by 刘虎 on 16/7/29.
+//  Copyright © 2016年 刘虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyImage
+//
+//  Created by 刘虎 on 16/7/29.
+//  Copyright © 2016年 刘虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
